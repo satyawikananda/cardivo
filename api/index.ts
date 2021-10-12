9a741f8d94ab89799aea86c5cbb19d3fbf1fff98
Alfian Rikzandi <denydirgantara94@gmail.com>
2021-10-12 16:59:35 +0700

    chore: add option disable animation
    - add `__disableAnimation` on `ParsedRequest` interface
    - add Ternary Operator animation option in card.ts
    - add disableAnimation query params

--- api/index.ts
+++ api/index.ts
@@ -4,13 +4,14 @@ import toBase64ImageUrl from "../utils/toBase64"
 
 export default async function render(req: NowRequest, res: NowResponse) {
     try {
-        const { name, description, image, backgroundColor, iconColor, site, fontColor, pattern, opacity, colorPattern, instagram, linkedin, github, twitter  } = req.query;
+        const { name, description, image, backgroundColor, iconColor, site, fontColor, pattern, opacity, colorPattern, instagram, linkedin, github, twitter, disableAnimation  } = req.query;
         
         const _linkedin = linkedin ?? ''
         const _instagram = instagram ?? ''
         const _twitter = twitter ?? ''
         const _github = github ?? ''
         const _site = site ?? ''
+        const __disableAnimation = Boolean(disableAnimation) ?? false
         const _image = await toBase64ImageUrl(image)
 
         const card = cardRender({
@@ -28,6 +29,7 @@ export default async function render(req: NowRequest, res: NowResponse) {
             _linkedin,
             _github,
             _twitter,
+            __disableAnimation
         });
 
         res.setHeader('Content-type', 'image/svg+xml');
